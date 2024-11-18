d6ecedd441562cf573d680dfca9c8dfb73039a8a
Amol Nakhate <106758417+amol64546@users.noreply.github.com>
2024-11-18 16:13:54 +0530

    Update folderScan.sh

--- folderScan.sh
+++ folderScan.sh
@@ -1,1 +1,24 @@
-trivy fs $path
+#!/bin/bash
+
+echo "Folder Path: $PATH"
+echo "SchemaId: $SCHEMA_ID"
+echo "Server URL: $SERVER_URL"
+
+if [ -z "$PATH" ] || [ -z "$SCHEMA_ID" ] || [ -z "$SERVER_URL" ] || [ -z "$BEARER_AUTH" ] ; then
+  echo "Error: Missing required environment variables."
+  exit 1
+fi
+
+# Step 1: Run Trivy and capture JSON output
+json_output=$(trivy fs "$PATH" --format json)
+#echo "$json_output"
+
+# Step 2: Make the API call using the captured JSON output
+response=$(curl --silent --location '$SERVER_URL/tf-entity-ingestion/v1.0/schemas/$SCHEMA_ID/instance?upsert=true' \
+ --header 'Content-Type: application/json' \
+ --header 'Authorization: "$BEARER_AUTH" \
+ --data "$json_output")
+
+# Step 3: Print the API response
+echo "API Response:"
+echo "$response"
