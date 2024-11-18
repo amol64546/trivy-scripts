3c4024a012ddd1a47e3e3a2fe44a0c90bd81fae4
Amol Nakhate <106758417+amol64546@users.noreply.github.com>
2024-11-18 16:56:25 +0530

    Update repoScan.sh

--- repoScan.sh
+++ repoScan.sh
@@ -1,24 +1,29 @@
 #!/bin/bash
 
+# Step 0: Print the input variables
 echo "Repository URL: $REPO_URL"
 echo "SchemaId: $SCHEMA_ID"
 echo "Server URL: $SERVER_URL"
 
-if [ -z "$REPO_URL" ] || [ -z "$SCHEMA_ID" ] || [ -z "$SERVER_URL" ] || [ -z "$BEARER_AUTH" ] ; then
+
+# Step 1: Validate required environment variables
+if [ -z "$REPO_URL" ] || [ -z "$SCHEMA_ID" ] || [ -z "$SERVER_URL" ] || [ -z "$BEARER_AUTH" ]; then
   echo "Error: Missing required environment variables."
   exit 1
 fi
 
-# Step 1: Run Trivy and capture JSON output
+# Step 2: Run Trivy and capture JSON output
 json_output=$(trivy repo "$REPO_URL" --format json)
-#echo "$json_output"
 
-# Step 2: Make the API call using the captured JSON output
-response=$(curl --silent --location '$SERVER_URL/tf-entity-ingestion/v1.0/schemas/$SCHEMA_ID/instance?upsert=true' \
- --header 'Content-Type: application/json' \
- --header 'Authorization: "$BEARER_AUTH" \
- --data "$json_output")
+if [ $? -ne 0 ]; then
+  echo "Error: Failed to run Trivy scan."
+  exit 1
+fi
 
-# Step 3: Print the API response
-echo "API Response:"
-echo "$response"
+# Step 3: Make the API call using the captured JSON output
+echo "Api call to: ${SERVER_URL}/tf-entity-ingestion/v1.0/schemas/${SCHEMA_ID}/instance?upsert=true"
+response=$(curl --silent --location \
+  "${SERVER_URL}/tf-entity-ingestion/v1.0/schemas/${SCHEMA_ID}/instance?upsert=true" \
+  --header "Content-Type: application/json" \
+  --header "Authorization: $BEARER_AUTH" \
+  --data "$json_output")
