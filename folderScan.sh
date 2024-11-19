4465f800d93f0f33e273f16179033198f1a74056
Amol Nakhate <106758417+amol64546@users.noreply.github.com>
2024-11-19 18:52:57 +0530

    Update folderScan.sh

--- folderScan.sh
+++ folderScan.sh
@@ -6,21 +6,39 @@ if [ -z "$PATH" ] || [ -z "$SCHEMA_ID" ] || [ -z "$SERVER_URL" ] || [ -z "$BEARE
   exit 1
 fi
 
-# Step 2: Run Trivy and capture JSON output
-json_output=$(trivy fs "$PATH" --format json)
+raw_output=$(trivy fs "$PATH" --format json)
 
 if [ $? -ne 0 ]; then
   echo "Error: Failed to run Trivy scan."
   exit 1
 fi
 
+json_output=$(echo "$raw_output" | sed -n '/^{/,$p')
+
+
+# Function to add a random "id" field to JSON
+add_id_to_json() {
+  local input_json="$1"
+  
+  # Generate a random alphanumeric ID
+  random_id=$(uuidgen | tr -d '-' | head -c 9)
+
+  # Add the "id" field to the JSON using jq
+  updated_json=$(echo "$input_json" | jq --arg id "$random_id" '. + {id: $id}')
+  
+  echo "$updated_json"
+}
+
+# Call the function and print the updated JSON
+updated_json=$(add_id_to_json "$json_output")
+
+
 # Step 3: Make the API call using the captured JSON output
 response=$(curl --silent --location \
   "$SERVER_URL/tf-entity-ingestion/v1.0/schemas/$SCHEMA_ID/instance?upsert=true" \
   --header "Content-Type: application/json" \
   --header "Authorization: $BEARER_AUTH" \
-  --data "$json_output")
+  --data "$updated_json")
 
 # Step 3: Print the API response
-echo "API Response:"
 echo "$response"
