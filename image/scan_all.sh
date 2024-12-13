450c88a88901d3a3f9f44947b6a2a545b3ace586
Amol Nakhate <106758417+amol64546@users.noreply.github.com>
2024-12-13 12:15:26 +0530

    Update scan_all.sh

--- image/scan_all.sh
+++ image/scan_all.sh
@@ -13,22 +13,6 @@ if [ $? -ne 0 ]; then
   exit 1
 fi
 
-json_output=$(echo "$raw_output" | sed -n '/^{/,$p')
+updated_json=$(echo "$raw_output" | sed -n '/^{/,$p')
 
-
-# Function to add a random "id" field to JSON
-add_id_to_json() {
-  local input_json="$1"
-  
-  # Generate a random alphanumeric ID
-  random_id=$(uuidgen | tr -d '-' | head -c 9)
-
-  # Add the "id" field to the JSON using jq
-  updated_json=$(echo "$input_json" | jq --arg id "$random_id" '. + {id: $id}')
-  
-  echo "$updated_json" | jq .
-}
-
-# Call the function and print the updated JSON
-updated_json=$(add_id_to_json "$json_output")
-echo "$updated_json" | jq .
+echo "$updated_json"
