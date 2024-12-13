ea48b46614cf8330e59992aa2749eceb486efee8
Amol Nakhate <106758417+amol64546@users.noreply.github.com>
2024-12-13 12:30:39 +0530

    Update scan_all.sh

--- image/scan_all.sh
+++ image/scan_all.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
 
-# Step 1: Validate required environment variables
-# if [ -z "$IMAGE" ]; then
-#   echo "Error: Missing required environment variables."
-#   exit 1
-# fi
-
-raw_output=$(trivy image "$IMAGE" --format json)
-
-if [ $? -ne 0 ]; then
-  echo "Error: Failed to run Trivy scan."
-  exit 1
-fi
-
-updated_json=$(echo "$raw_output")
+updated_json=$(trivy image "$IMAGE" --format json)
 
 echo "$updated_json"
