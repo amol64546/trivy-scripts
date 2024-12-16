b86734bcb621e7ba80b066202cc553c6df395482
Amol Nakhate <106758417+amol64546@users.noreply.github.com>
2024-12-16 16:39:54 +0530

    Update scan.sh

--- repository/scan.sh
+++ repository/scan.sh
@@ -17,21 +17,5 @@ fi
 # Run Trivy command and get JSON output
 json_output=$(eval "$TRIVY_COMMAND --format json")
 
-# Function to add a random "id" field to JSON
-add_id_to_json() {
-  local input_json="$1"
-  
-  # Generate a random alphanumeric ID
-  random_id=$(uuidgen | tr -d '-' | head -c 9)
-
-  # Add the "id" field to the JSON using jq
-  updated_json=$(echo "$input_json" | jq --arg id "$random_id" '. + {id: $id}')
-  
-  echo "$updated_json"
-}
-
-# Add ID to JSON output
-updated_json=$(add_id_to_json "$json_output")
-
 # Output the updated JSON
-echo "$updated_json"
+echo "$json_output"
