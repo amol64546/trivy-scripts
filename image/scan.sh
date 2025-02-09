3e396fb609a6700bbc8f0a454b065586af174bbe
Amol N <amol64546@gmail.com>
2025-02-09 20:01:27 +0530

    refactor properties

--- image/scan.sh
+++ image/scan.sh
@@ -2,11 +2,15 @@
 
 # Parse flag inputs
 if [ "$SCANNER_FLAG" == "misconfig" ]; then
-    TRIVY_COMMAND="trivy image --image-config-scanners misconfig $IMAGE"
+    TRIVY_COMMAND="trivy image --scanners misconfig --image-config-scanners misconfig $IMAGE"
 elif [ "$SCANNER_FLAG" == "secret" ]; then
-    TRIVY_COMMAND="trivy image --image-config-scanners secret $IMAGE"
+    TRIVY_COMMAND="trivy image --scanners secret --image-config-scanners secret $IMAGE"
+elif [ "$SCANNER_FLAG" == "vuln" ]; then
+    TRIVY_COMMAND="trivy image --scanners vuln $IMAGE"
+elif [ "$SCANNER_FLAG" == "license" ]; then
+    TRIVY_COMMAND="trivy image --scanners license $IMAGE"
 else
-    TRIVY_COMMAND="trivy image $IMAGE"
+    TRIVY_COMMAND="trivy image --scanners vuln,misconfig,secret,license --image-config-scanners misconfig,secret $IMAGE"
 fi
 
 # Run Trivy command and get JSON output
--- repository/scan.sh
+++ repository/scan.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
-
 # Parse flag inputs
 if [ "$SCANNER_FLAG" == "misconfig" ]; then
-    TRIVY_COMMAND="trivy fs --scanners misconfig $PATH"
+    TRIVY_COMMAND="trivy repo --scanners misconfig $PATH"
 elif [ "$SCANNER_FLAG" == "secret" ]; then
-    TRIVY_COMMAND="trivy fs --scanners secret $PATH"
+    TRIVY_COMMAND="trivy repo --scanners secret $PATH"
 elif [ "$SCANNER_FLAG" == "license" ]; then
-    TRIVY_COMMAND="trivy fs --scanners license $PATH"
+    TRIVY_COMMAND="trivy repo --scanners license $PATH"
 elif [ "$SCANNER_FLAG" == "vuln" ]; then
-    TRIVY_COMMAND="trivy fs --scanners vuln $PATH"
+    TRIVY_COMMAND="trivy repo --scanners vuln $PATH"
 else
-    TRIVY_COMMAND="trivy fs $PATH"
+    TRIVY_COMMAND="trivy repo --scanners misconfig,vuln,license,secret $PATH"
 fi
 
 # Run Trivy command and get JSON output
